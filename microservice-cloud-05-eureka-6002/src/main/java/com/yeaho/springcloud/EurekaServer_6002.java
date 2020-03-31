835461e42d12960b001ed1554fafc973df237b2a
yeaho <yeaho_lee@qq.com>
2020-03-31 21:46:39 +0800

    eureka集群

---
+++ microservice-cloud-05-eureka-6002/src/main/java/com/yeaho/springcloud/EurekaServer_6002.java
@@ -0,0 +1,21 @@
+package com.yeaho.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author: yeaho_lee
+ * @description:
+ * @date: 12:18 2020/3/31
+ */
+@SpringBootApplication
+/**
+ * 服务注册中心
+ */
+@EnableEurekaServer
+public class EurekaServer_6002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer_6002.class, args);
+    }
+}
