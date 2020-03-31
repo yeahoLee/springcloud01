7fe6d6cdbf4624e6d446b062d5a6081862723f42
yeaho <yeaho_lee@qq.com>
2020-03-31 18:20:39 +0800

    EurekaServer_6001

--- provider/src/main/java/com/yeaho/springcloud/ProductProvider_8001.java
+++ provider/src/main/java/com/yeaho/springcloud/ProductProvider_8001.java
@@ -3,9 +3,11 @@ package com.yeaho.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @MapperScan("com.yeaho.springcloud.mapper")
+@EnableEurekaClient
 public class ProductProvider_8001 {
     public static void main(String[] args) {
         SpringApplication.run(ProductProvider_8001.class, args);
