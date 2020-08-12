ab370dfd227c9d9d867004ec2f4b08d3e01244ae
yeaho <yeaho_lee@qq.com>
2020-08-12 22:06:03 +0800

    feign

--- microservice-cloud-07-consumer-product-feign/src/main/java/springcloud/service/ProductClientService.java
+++ microservice-cloud-07-consumer-product-feign/src/main/java/springcloud/service/ProductClientService.java
@@ -1,7 +1,6 @@
 package springcloud.service;
 
 import com.yeaho.springcloud.entity.Product;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
